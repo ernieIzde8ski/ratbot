95fbffe16ab3c8f49eba75b647ac257ab3003169
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2020-05-10 03:27:57 -0400

    PEP 8 compliancy update
    PEP 8 compliancy, log on/log off logs to a given channel, shut down/restart commands, and a bunch to look better than before

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
-# bot.py 
+# bot.py
 # ^ dunno what it means but i assume it serves a purpose
+# here's all the necessary imports and a definition
 import os
 import sys
 import discord
@@ -7,28 +8,44 @@ import asyncio
 from datetime import datetime
 import random
 import config
-
 client = discord.Client()
 
-def is_in(symbol, lst):
-    return any(symbol in x for x in lst)
+# this seems necessary for some dumb reason
+item = 'item'
+lst = 'lst'
 
-def restart():
+
+# really shortens down some useless lines
+def is_in(item, lst):
+    return any(word in item for word in lst)
+
+# defining functions for shutdown and restart
+async def die():
+    channel = client.get_channel(config.statusChannel)
+    await channel.send('<:offline:708886391672537139> shutting down...')
+    print('shutting down')
+    sys.exit()
+async def restart():
+    channel = client.get_channel(config.statusChannel)
+    await channel.send('<:restarting:708887315853869087> restarting...')
+    print('restarting...')
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
 
+# finally, some god damnned events
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    channel = client.get_channel(config.statusChannel)
+    await channel.send('<:online:708885917133176932> online!')
 
 
 @client.event
 async def on_message(message):
 
     auth = message.author
-
-    if auth == client.user or message.author.bot: #obvious return is obvious
+    if auth == client.user or message.author.bot:  # obvious return is obvious
         return
 
     now = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
@@ -36,44 +53,62 @@ async def on_message(message):
     msg = message.content
     msgID = message.id
     guild = message.guild
-    guildID = message.guild.id
-    cn = message.channel
-    cnName = message.channel.name
-    cnID = message.channel.id
-    coolMsgIDs = str(guildID) + '.' + str(cnID) + '.' + str(msgID)
+    chan = message.channel
+    coolMsgIDs = str(guild.id) + '.' + str(chan.id) + '.' + str(msgID)
+    slursExist = is_in(item=msg, lst=config.slursList)
 
-    if authID == 368780147563823114 and msg.lower() == 'ernie':
-        await cn.send(spokesperson)
+    # make rat shut down
+    if msg == 'die, rat':
+        await chan.send('alright chief, bring me back soon')
+        await die()
         return
-
-    elif 'retard' in msg: #correct ratards
-        await cn.send('*ratard')
-        return
-
-    elif any(word in msg for word in config.slursList) and any(word in str(message.guild.id) for word in config.guildOptIn): #detect slurs & make them only work in whichever servers have opted in
-        print('loser detected')
-        await cn.send('loser ' + message.author.mention)
-        return
-
-    elif msg.startswith('ratmin ') or msg == 'ratmin' or msg == 'ratmint':#non functioning ratmin commands
-        print(f'[{now}] ratmin in #{cnName} of {str(guild)} ({coolMsgIDs})')
-        if authID == config.ratminID:
-            if msg == 'ratmin sendhere':#don't bother getting this to work
-                await cn.send(notFunctional)
-            else:
-                await cn.send('bruh that\'s not a command')
-                return
-        else:#if someone isn't ratmin, tell them to shut up
-            await cn.send(random.choice(config.shutUp))
-            return
+    # make rat restart
     elif msg == 'ratstart' and authID == config.ratminID:
-        restart()
-    elif msg.startswith('rat'):#respond to rat with rat
-        print(f'[{now}] rat in #{cnName} of {str(guild)} ({coolMsgIDs})')
-        await cn.send('rat')
+        await chan.send('alright chief, hope i survive')
+        await restart()
         return
+
+    # respond when myer says my name
+    elif authID == 368780147563823114 and msg.lower() == 'ernie':
+        await chan.send(spokesperson)
+        return
+
+    elif 'retard' in msg:  # correct ratards
+        await chan.send('*ratard')
+        return
+
+    # detect slurs & make them only work in whichever servers have opted in
+    elif slursExist and is_in(item=guild.id, lst=config.guildOptIn):
+        print('loser detected')
+        await chan.send('loser ' + message.author.mention)
+        return
+
+    # ratmin commands (non-functioning)
+    elif msg.startswith('ratmin ') or msg == 'ratmin' or msg == 'ratmint':
+        print(f'[{now}] ratmin in #{chan.name} of {str(guild)} ({coolMsgIDs})')
+        if authID == config.ratminID:
+            if msg == 'ratmin sendhere':  # don't bother getting this to work
+                await chan.send(notFunctional)
+            else:
+                await chan.send('bruh that\'s not a command')
+                return
+        else:  # if someone isn't ratmin, tell them to shut up
+            await chan.send(random.choice(config.shutUp))
+            return
+
+    # the ultimate purpose: respond to rat with rat
+    elif msg.startswith('rat'):
+        print(f'[{now}] rat in #{chan.name} of {str(guild)} ({coolMsgIDs})')
+        await chan.send('rat')
+        return
+
+#    # this is for debugging
+#    elif authID == config.ratminID:
+#        print('end reached')
+
+    # any other message should reach here
     else:
-#       print('else reached; message is ' + msg) #for debugging
         return
 
 client.run(config.TOKEN)
+# clouds
