91c9869b5d81ddae5fa2839e96241feb7650395e
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2020-05-10 00:12:57 -0400

    Update config.py
    minor revisions

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ TOKEN = 'your bot token'
 shutUp = ['shut up liberal', 'what do you want', 'i am your god who the hell do you think you are', 'wot', 'wut', 'WHAT DO YOU WANT', 'wot you want, pathetic mortal', 'lower your shields and surrender your ships because this is the end for your civilization', 'death to capitalism & you', 'do you have nothing better with your life to do right now', 'what', 'что', 'look i don\'t know what you think is a productive use of time but this is not it', 'how HIGH do you even have to BE', 'do you ever think of the utterly meaningless impact your messages right here will ever have', 'couldn\'t you be out there being productive rn', 'I DONT CARE ABOUT YOU', 'you look your best away from the keyboard; never forget', 'https://en.wikipedia.org/wiki/Shut_up', 'https://www.youtube.com/watch?v=KRB-iHGHSqk', 'https://www.youtube.com/watch?v=RwC9CP_2YKE', 'this is explicitly a bot admin command what do you want from me', '']
 slursList = ['nigger', 'nigga', 'niga', 'negroid', 'jigaboo', 'cracker', 'faggot', 'fag', 'slut', 'whore', 'gook', 'ExampleSlur']
 guildOptIn = ['488475203303768065', '682704934621282307']
-notFunctional = 'this functional is not currently functional. please fix it soon eridan man'
+ratminID = 302956027656011776
+statusChannel = 708882977202896957 
+
+# these are less intended to be configured but more intended to shorten how bot.py looks
 spokesperson = 'i am his spokesperson, what do you want of him'
-ratminID = your user id (int)
+notFunctional = 'this functional is not currently functional. please fix it soon eridan man'
