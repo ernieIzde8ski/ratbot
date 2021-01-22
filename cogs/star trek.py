b28e94da6276180cf81d9d68c7df13d830074c1f
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-01-22 15:40:22 -0500

    Add files via upload

--- cogs/star trek.py
+++ cogs/star trek.py
@@ -1,25 +1,48 @@
-import random
 import discord
-from discord.ext import commands
+import discord.ext.commands as commands
 import modules.SentenceGenerator as SentenceGenerator
+from typing import Optional
 
 
 class startrek(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.generator = SentenceGenerator.loadGenerator("modules/StarTrek.txt")
+        try:
+            self.tos_generator = SentenceGenerator.loadGenerator("modules/StarTrek.txt")
+            self.tng_generator = SentenceGenerator.loadGenerator("modules/StarTrekTNG.txt")
+        except:
+            print("Lol TNG Proaly Not working")
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def load_trek(self, ctx):
-        self.generator = SentenceGenerator.loadGenerator("modules/StarTrek.txt")
+        """reloads the star trek generator, in case changes occurred to StarTrek.txt"""
+        self.tos_generator = SentenceGenerator.loadGenerator("modules/StarTrek.txt")
+        self.tng_generator = SentenceGenerator.loadGenerator("modules/StarTrekTNG.txt")
         await ctx.send('Reloaded! probably')
 
+    @commands.command(aliases = ["tos"])
+    async def random_tos(self, ctx, count: int = 3):
+        """Generates a randomized Star Trek: TOS plot."""
+
+        if not (1 <= count <= 5):
+            await ctx.send(f"1-5 pls (Not {count} Bro)")
+            return
+        for i in range(count):
+            await ctx.send(self.tos_generator.generate())
+
+    @commands.command(aliases = ["tng"])
+    async def random_tng(self, ctx, count: int = 3):
+        """Generates a randomized Star Trek: TNG plot."""
+        if not (1 <= count <= 5):
+            await ctx.send(f"1-5 pls (Not {count} Bro)")
+            return
+        for i in range(count):
+            await ctx.send(self.tng_generator.generate())
+
     @commands.command()
-    async def random_tos(self, ctx):
-        try:
-            await ctx.send(self.generator.generate())
-        except:
-            await ctx.send('An error occurred! so sad')
+    async def borg(self, ctx, *, item: str):
+        """Generates a borg phrase dependent on your input."""
+        await ctx.send(f"We are the {item}. Lower your shields and surrender your ships. We will add your biological and technological distinctiveness to our own. Your culture will adapt to service us. Resistance is futile.", allowed_mentions=discord.AllowedMentions.none())
 
 
 def setup(bot):
