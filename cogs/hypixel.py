9b9ff6d0e34a4dee83984e4984f0559b5fcd02d9
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-01-22 15:23:45 -0500

    Add files via upload

---
+++ cogs/hypixel.py
@@ -0,0 +1,23 @@
+# this seems to like to generate a weird error a lot of the time:
+#   C:\Users\izdeb\AppData\Local\Programs\Python\Python39\lib\site-packages\hypixelaPY\objects\stats.py:57: RuntimeWarning: coroutine 'get_api_stats' was never awaited
+#     self.ratio = Ratio(self.kills, self.deaths)
+# myer seems to be aware of it ? 
+import discord
+import discord.ext.commands as commands
+import asyncio
+from hypixelaPY import Hypixel
+
+from config import api_key
+hypixel = Hypixel(api_key)
+
+class hypixelbasedlevels(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def bm_hypixel(self, ctx, player_name):
+        player = await hypixel.player.get(name=player_name)
+        await ctx.send("based") if player.bedwars.games_played < 10 else await ctx.send(f"cringe ({player.bedwars.games_played} Cringegamecount)")
+
+def setup(bot):
+    bot.add_cog(hypixelbasedlevels(bot))
