e3ab1a1c0ea82234380eed7c3d51ce4174091d09
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-01-25 11:09:59 -0500

    Bruh Lol

--- cogs/core/mentions.py
+++
@@ -1,7 +0,0 @@
-from discord import AllowedMentions
-
-none = AllowedMentions.none()
-all = AllowedMentions.all()
-everyone = AllowedMentions(everyone=True, users=False, roles=True)
-users = AllowedMentions(everyone=False, users=True, roles=False)
-roles = AllowedMentions(everyone=False, users=False, roles=True)
