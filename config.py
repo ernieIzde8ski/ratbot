d1010941bbd6952bf6c47c3462dd3345daed2413
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-01-30 22:02:18 -0500

    Clarified variable meanings in config

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
+# guilds which have opted into tenor slaying
+# only relevant if you load cogs.on_message.respuestas
 guildOptIn = [488475203303768065, 526207286067068928, 534875827398246451]
+# bot admin user id, mostly extant by now
 ratmin_id = 302956027656011776
+# channel where startup is logged
 statusChannel = 708882977202896957
+# channel where miscellaneous actions [i.e. dm messages to rat] are logged
 logChannel = 715297562613121084
+# cogs that load alongside the bot
 enabledcogs = ["cogs.admin", "cogs.misc", "cogs.star trek", "cogs.on_message.dms", "cogs.on_message.PIPI",
                "cogs.on_message.corrections", "cogs.on_message.ratsponding"]
-# line when responding to my name - must be lowercase
+# bot admin name - must be lowercase
 adminname = "ernie"
+# line when responding to bot admin name
 spokesperson = "i am his spokesperson, what do you want of him"
 
 
