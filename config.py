64554a348b2313fbaa6381ed848799b5951b0721
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-01-30 21:21:05 -0500

    nvm it was just the wrong closing bracket

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ ratmin_id = 302956027656011776
 statusChannel = 708882977202896957
 logChannel = 715297562613121084
 enabledcogs = ["cogs.admin", "cogs.misc", "cogs.star trek", "cogs.on_message.dms", "cogs.on_message.PIPI",
-               "cogs.on_message.corrections", "cogs.on_message.ratsponding")]
+               "cogs.on_message.corrections", "cogs.on_message.ratsponding"]
 # line when responding to my name
 adminname = "Ernie"
 spokesperson = "i am his spokesperson, what do you want of him"
