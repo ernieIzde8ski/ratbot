a62df4decb37d1a47592575ded25c5a7c41d7ff0
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-01-31 19:22:39 -0500

    bot.py is now legible

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# i dont understand half of whats here myself
+# legible ?
 # good luck
 # here's all the necessary imports
 from datetime import datetime
@@ -10,9 +10,9 @@ import discord.ext.commands as commands
 
 import config
 
-bot = commands.Bot(command_prefix=["r.", "rat! "], allowed_mentions=discord.AllowedMentions.none())
-intents = discord.Intents.all()
-
+bot = commands.Bot(command_prefix=["r.", "rat! "],
+                   allowed_mentions=discord.AllowedMentions.none(),
+                   intents=discord.Intents.all())
 
 # Cog ?
 if __name__ == "__main__":
@@ -21,26 +21,6 @@ if __name__ == "__main__":
             bot.load_extension(extension)
         except commands.ExtensionError as e:
             print(f"{e.__class__.__name__}: {e}")
-
-
-def now(): return str(datetime.today().strftime("%d-%m-%Y %H:%M:%S"))
-
-
-def _removeNonAscii(s): return "".join(i for i in s if ord(i) < 384)
-
-
-def msg_log(msg, msg_type):
-    # if in DMs
-    if not msg.guild:
-        embed = discord.Embed(title=f"DM Message from <@{msg.author.id}> ({msg.author})",
-                              description=msg.content, timestamp=msg.created_at)
-        return embed
-    # if not
-    if msg.guild.id:
-        embed = discord.Embed(title=f"{msg_type} in #{msg.channel.name} of {msg.guild.name}",
-                              url=f"https://discordapp.com/channels/{msg.guild.id}/{msg.channel.id}/{msg.id}",
-                              description=msg.content, timestamp=msg.created_at)
-        return embed
 
 
 @bot.event
