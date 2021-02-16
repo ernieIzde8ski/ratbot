c77e22357bcc958ef40db8eb647b05b0cae2f8af
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-02-15 21:15:27 -0500

    appeasing myer

--- cogs/on_message/ernie_censorship.py
+++ cogs/on_message/ernie_censorship.py
@@ -8,11 +8,11 @@ class Censorship(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, ctx):
-        if ctx.author.bot: return
-        if ctx.author.id != 232706427045543936 and ctx.channel.id == 811023978045898822:
-            await ctx.delete()
-            await ctx.channel.send(f"use reactions Idiot {ctx.author.mention}",
+    async def on_message(self, message):
+        if message.author.bot: return
+        if message.author.id != 232706427045543936 and message.channel.id == 811023978045898822:
+            await message.delete()
+            await message.channel.send(f"use reactions Idiot {message.author.mention}",
                                    allowed_mentions=discord.AllowedMentions(users=True),
                                    delete_after=3)
         else: return
