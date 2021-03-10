32adb3180559a986417ceeeff9c7920f16e031ce
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-03-10 15:50:29 -0500

    working cog

--- bot.py
+++ bot.py
@@ -3,14 +3,22 @@ import discord
 import asyncio
 
 import secrets
-#import config
+import config
 
 bot = commands.Bot(command_prefix="r,")
+
+
+for Cogule in config.cogules:
+      try:
+            bot.load_extension(Cogule)
+      except commands.ExtensionError as err:
+            print(f"{err.__class__.__name__}: {err}")
 
 
 @bot.event
 async def on_ready():
       channel = bot.get_channel(708882977202896957)
       await channel.send("Ya Estoy En Línea Pendejos")
+
 
 bot.run(secrets.token)
