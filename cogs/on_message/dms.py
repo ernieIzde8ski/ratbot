a9d3a431519e87de60b2b9608e9bcd630ba9bfd3
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-03-19 16:09:59 -0400

    added some comments and stuff

--- cogs/on_message/dms.py
+++ cogs/on_message/dms.py
@@ -8,24 +8,28 @@ class DM(commands.Cog):
         self.bot = bot
         self.latest = None
 
-    @commands.Cog.listener("on_message")
-    async def log_message(self, msg):
+    @commands.Cog.listener()
+    async def on_message(self, msg):
+        # pass only if in direct messages
         if msg.guild: return
+        # log own messages as an embed in the proper channel
         if msg.author == self.bot.user:
             embed = discord.Embed(title=f"Direct Message → {msg.channel.recipient} ({msg.channel.recipient.id})",
                                   description=msg.content, timestamp=msg.created_at, color=discord.Color.orange())
-            await self.bot.config.channels.log.send(embed=embed)
-        if msg.author.bot:
-            return
+            return await self.bot.config.channels.log.send(embed=embed)
+        # log message only if is not bot user
+        elif msg.bot.user: return
         else:
             embed = discord.Embed(title=f"Direct Message — {msg.author} ({msg.author.id})",
                                   description=msg.content, timestamp=msg.created_at, color=discord.Color.dark_blue())
+            # also save for later
+            self.latest = msg
             await self.bot.config.channels.log.send(embed=embed)
 
     @commands.command(aliases=["vessel", "wessel"])
     @commands.is_owner()
     async def echo(self, ctx, messageable: Union[commands.TextChannelConverter, commands.UserConverter, int, None], *,
-                     text: str):
+                   text: str):
         """Bogstandard echo command"""
         if type(messageable) is int or not messageable:
             messageable = self.bot.get_channel(messageable) if (type(messageable) is int) else ctx.channel
