dce70596b72b08f51e852c0b0236a5d9c86a10c2
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-03-27 19:57:34 -0400

    Gobi correction

--- cogs/randomized.py
+++ cogs/randomized.py
@@ -32,7 +32,7 @@ class Randomized(commands.Cog):
             await self.bm_channel.send()
 
     @commands.command(aliases=["gm", "gobi"])
-    async def gobi_meter(self, ctx, phrase: str):
+    async def gobi_meter(self, ctx, *, phrase: str):
         random.seed(self.bot.static.remove_strange_chars(phrase.lower()))
         percent = round(random.random()*100, 1)
         await ctx.channel.send(f"\"{phrase}\" is {percent}% Gobi")
