ac6c48077cdba7c6c51f0853373fa9d18943f731
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-03-30 11:40:31 -0400

    Fix bug

--- cogs/uncategorized.py
+++ cogs/uncategorized.py
@@ -18,8 +18,8 @@ class Uncategorized(commands.Cog):
         embed = Embed(
             name="Awesome?",
             description=f"[GitHub](https://github.com/ernieIzde8ski/ratbot)\n"
-                        f"[Bot Invite]({utils.oauth_url(self.bot.id, permissions=Permissions(8))})\n"
-                        f"[bot invite but you have to think about it]({utils.oauth_url(self.bot.id, permissions=Permissions(201714752))}\n"
+                        f"[Bot Invite]({utils.oauth_url(self.bot.user.id, permissions=Permissions(8))})\n"
+                        f"[bot invite but you have to think about it]({utils.oauth_url(self.bot.user.id, permissions=Permissions(201714752))}\n"
                         f"[\"Support Server\"](https://discord.gg/cHZYahK)\n"
                         f"[Good song](https://www.youtube.com/watch?v={choice(ctx.bot.config.songs)})",
             color=ctx.author.color, timestamp=ctx.message.created_at
