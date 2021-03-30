f1d2a373c30c4da502bbae019b7f891ab6e8d37e
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-03-30 11:36:22 -0400

    Used discord.utils function instead of the link

--- cogs/uncategorized.py
+++ cogs/uncategorized.py
@@ -1,24 +1,25 @@
 from random import choice
 
-import discord
+from discord import Embed, Permissions, utils
 from discord.ext import commands
 
 
 class Uncategorized(commands.Cog):
-    """There is only one command here Doe?"""
+    """There is only one command here Doe?
+    Yea"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["support", "info"])
+    @commands.command(aliases=["support", "info", "invite"])
     async def information(self, ctx):
         """Provides general/support information"""
         bot_owner = (await commands.Bot.application_info(self.bot)).owner
-        embed = discord.Embed(
+        embed = Embed(
             name="Awesome?",
             description=f"[GitHub](https://github.com/ernieIzde8ski/ratbot)\n"
-                        f"[Bot Invite](https://discordapp.com/api/oauth2/authorize?client_id=466737001832382464&permissions=8&scope=bot)\n"
-                        f"[bot invite but you have to think about it](https://discordapp.com/api/oauth2/authorize?client_id=466737001832382464&permissions=201714752&scope=bot)\n"
+                        f"[Bot Invite]({utils.oauth_url(self.bot.id, permissions=Permissions(8))})\n"
+                        f"[bot invite but you have to think about it]({utils.oauth_url(self.bot.id, permissions=Permissions(201714752))}\n"
                         f"[\"Support Server\"](https://discord.gg/cHZYahK)\n"
                         f"[Good song](https://www.youtube.com/watch?v={choice(ctx.bot.config.songs)})",
             color=ctx.author.color, timestamp=ctx.message.created_at
