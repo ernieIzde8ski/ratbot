773f6bab5adcabe5f847c5d983c8b0fd10d18156
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-03-30 20:22:34 -0400

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,9 @@
+# https://discord.com/developers/applications/[ID]/bot → "Click to Reveal Token"
+token = "insert bot token here"
+# run /api on the hypixel server
+# obviously only applicable if intending on running cogs/hypixel.py,
+# but otherwise keeping the cog enabled without a valid token will error out safely
+hypixel_api_key = "insert hypixel api key here"
+# subscribe to the free "Current Weather Data" API on https://openweathermap.org/api
+# same applies as from hypixel_api_key but with cogs/on_member_update/Armenium.py
+weather_api_key = "insert weather api key here"
