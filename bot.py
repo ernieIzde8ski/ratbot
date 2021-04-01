ef1d33b8a8663c523f2793d143e504909933e40f
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-04-01 12:17:54 -0400

    move config files to folder

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@
 # good luck
 # here's all the necessary imports
 import asyncio
-from enabled_cogs import enabled_cogs
+from configs.enabled_cogs import enabled_cogs
 import discord
 import discord.ext.commands as commands
 import hypixelaPY.exceptions as hypixele
 from hypixelaPY import Hypixel
 
-from config import Config
-from secrets import token, hypixel_api_key
+from configs.config import Config
+from configs.secrets import token, hypixel_api_key
 from static import Static
 
 intentions = discord.Intents.all()
