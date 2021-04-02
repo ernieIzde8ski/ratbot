88fccd3fea9128f4182a449ff6402c6e35a93cdb
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-04-02 18:06:35 -0400

    Tupled

--- cogs/on_message/respuestas.py
+++ cogs/on_message/respuestas.py
@@ -4,15 +4,14 @@ from discord.ext import commands
 class Respuestas(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.tenor = ("https://giphy.com/gifs/", "https://tenor.com/view/")
 
     @commands.Cog.listener("on_message")
     async def censorship(self, msg):
         if msg.author.bot or not msg.guild: return
         if msg.channel.name != "rat":
             # kill tenor links lmao
-            if msg.guild.id in self.bot.config.guild_opt_in and (
-                    msg.content.startswith("https://giphy.com/gifs/") or msg.content.startswith(
-                "https://tenor.com/view/")):
+            if msg.guild.id in self.bot.config.guild_opt_in and msg.content.startswith(self.tenor):
                 await msg.channel.send(f"loser {msg.author.mention}")
                 await msg.delete()
 
