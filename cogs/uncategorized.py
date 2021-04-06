ebc4ab6bbbd79e671af66b83b1ff745c9f9bada3
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-04-06 16:58:52 -0400

    Preparé para nuevo cambios

--- cogs/uncategorized.py
+++ cogs/uncategorized.py
@@ -18,14 +18,20 @@ class Uncategorized(commands.Cog):
         embed = Embed(
             name="Awesome?",
             description=f"[GitHub](https://github.com/ernieIzde8ski/ratbot)\n"
-                        f"[Bot Invite]({utils.oauth_url(self.bot.user.id, permissions=Permissions(8))})\n"
-                        f"[bot invite but you have to think about it]({utils.oauth_url(self.bot.user.id, permissions=Permissions(201714752))})\n"
+                        f"[Invitame]({utils.oauth_url(self.bot.user.id, permissions=Permissions(8))})\n"
+                        f"[invitación pero necesitas pensar]({utils.oauth_url(self.bot.user.id, permissions=Permissions(201714752))})\n"
                         f"[\"Support Server\"](https://discord.gg/cHZYahK)\n"
                         f"[Good song](https://www.youtube.com/watch?v={choice(ctx.bot.config.songs)})",
             color=ctx.author.color, timestamp=ctx.message.created_at
-        ).set_author(name=bot_owner, icon_url=bot_owner.avatar_url)
+        ).set_author(
+            name=bot_owner,
+            icon_url=bot_owner.avatar_url
+        ).set_footer(
+            text="do `r.help command` for information on commands"
+        )
         await ctx.send(embed=embed)
 
 
 def setup(bot):
+    bot.remove_command("help") & print("Saying the Bye-Bye to Help Command")
     bot.add_cog(Uncategorized(bot))
