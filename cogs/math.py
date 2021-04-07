8e83f7dd932ac02356b3629605dff5fd9297610b
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-04-07 00:18:17 -0400

    Thinking Idea And Then The Bed

---
+++ cogs/math.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from discord.ext import commands
+
+
+class Math(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @staticmethod
+    def mean(ints):
+        if len(ints) > 0:
+            return f"{sum(ints)}/{len(ints)}={sum(ints) / len(ints)}"
+
+    @commands.command(aliases=["l"])
+    async def list_stats(self, ctx, *, lista: Union[list, tuple]):
+        mean = self.mean(lista)
+        return await ctx.channel.send(mean)
+
+
+def setup(bot):
+    bot.add_cog(Math(bot))
