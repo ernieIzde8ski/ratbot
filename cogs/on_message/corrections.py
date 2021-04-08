8b5cb4b70f7c477b6f7a1bc4648ab512b3248c3f
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-04-07 23:14:53 -0400

    (ExampleSlur, Armenium)

--- cogs/on_message/corrections.py
+++ cogs/on_message/corrections.py
@@ -4,15 +4,16 @@ from discord.ext import commands
 class Corrections(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.corrections_list = (
+        self.corrections_list = [
             ("bano", "Baño"),
             ("senor", "Señor"),
             ("senora", "Señora"),
             ("jalapeno", "Jalapeño"),
             ("canada", "Cañada"),
             ("canadian", "Cañadian"),
-            ("retard", "Ratard")
-        )
+            ("retard", "Ratard"),
+            ("ExampleSlur", "Armenium")
+        ]
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -20,7 +21,7 @@ class Corrections(commands.Cog):
         if message.guild and message.channel.name == "rat": return
         message_list = message.content.lower().split(" ")
         for x, y in self.corrections_list:
-            if x in message_list:
+            if x.lower() in message_list:
                 return await message.channel.send(f"did you mean: {y}")
 
 
