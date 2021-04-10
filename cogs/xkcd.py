f9702450fd498889126dfb72d12bc48e6f56af6a
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-04-10 16:03:09 -0400

    Mostly functional xkcd command

--- cogs/xkcd.py
+++ cogs/xkcd.py
@@ -1,19 +1,41 @@
 from typing import Union
-
+import aiohttp
 from discord.ext import commands
+from discord import Embed
 
 
 class XKCD(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @staticmethod
+    async def get_xkcd(xkcd_id: int):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"http://xkcd.com/{xkcd_id}/info.0.json") as resp:
+                return await resp.json()
+
+    @staticmethod
+    async def embed_constructor(data, color):
+        return Embed(
+            title=data['title'],
+            url=f"https://xckd.com/{data['num']}",
+            color=color
+        ).set_footer(
+            text=data['alt']
+        ).set_image(
+            url=data['img']
+        )
+
+
     @commands.command()
     async def xkcd(self, ctx, *, xkcd_id: Union[int, str, None]):
         idtype = xkcd_id.__class__.__name__
         if idtype == "int":
-            return await ctx.channel.send(f"https://xkcd.com/{xkcd_id}/")
+            xkcd_json = await self.get_xkcd(xkcd_id)
+            embed = await self.embed_constructor(xkcd_json, ctx.author.color)
+            await ctx.channel.send(embed=embed)
         elif idtype == "str":
-            await ctx.send(f"Sorry I Don't know how to handle looking for {xkcd_id} ( Yet)")
+            await ctx.send(f"Sorry I Don't know how to handle looking for `{xkcd_id}` ( Yet)")
         else:
             await ctx.send("Please provide Input")
 
