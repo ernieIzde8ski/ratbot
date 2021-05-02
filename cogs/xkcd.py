a02579027c65ab3b1f4c3538a83eb46513e2affd
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-05-01 21:59:42 -0400

    half broke xkcd update thanks to God knows

--- cogs/xkcd.py
+++ cogs/xkcd.py
@@ -1,10 +1,10 @@
+from random import randint
 from typing import Union
 
 import discord.ext.commands as commands
 from aiohttp import ClientSession
 from aiohttp.client_exceptions import ContentTypeError
 from discord import Embed
-from random import randint
 
 
 class XKCD(commands.Cog):
@@ -12,15 +12,15 @@ class XKCD(commands.Cog):
         self.bot = bot
 
     @staticmethod
-    async def get_xkcd(xkcd_id: Union[int, None]):
+    async def get_xkcd(xkcd_id: Union[int, None]) -> dict:
         async with ClientSession() as session:
             async with session.get("https://xkcd.com{}info.0.json".format(f"/{xkcd_id}/" if xkcd_id else "/")) as resp:
                 return await resp.json()
 
     @staticmethod
-    async def embed_constructor(data, color):
+    async def embed_constructor(data, color) -> Embed:
         return Embed(
-            title=data['title'],
+            title=f"{data['num']}: {data['title']}",
             url=f"https://xkcd.com/{data['num']}/",
             color=color
         ).set_footer(
@@ -28,6 +28,13 @@ class XKCD(commands.Cog):
         ).set_image(
             url=data['img']
         )
+
+    async def get_random_xkcd(self) -> dict:
+        latest_xkcd = (await self.get_xkcd(None))['num']
+        while True:
+            num = randint(1, latest_xkcd)
+            if num != 404: break
+        return await self.get_xkcd(num)
 
     @commands.group(invoke_without_command=True)
     async def xkcd(self, ctx, *, xkcd_id: Union[int, str] = 221):
@@ -54,13 +61,35 @@ class XKCD(commands.Cog):
     @xkcd.command()
     async def random(self, ctx):
         """get random xkcd"""
-        latest_xkcd = (await self.get_xkcd(None))['num']
-        while True:
-            num = randint(1, latest_xkcd)
-            if num != 404: break
-        xkcd_json = await self.get_xkcd(num)
-        embed = await self.embed_constructor(xkcd_json, ctx.author.color)
+        xkcd_dict = await self.get_random_xkcd()
+        embed = await self.embed_constructor(xkcd_dict, ctx.author.color)
         await ctx.send(embed=embed)
+
+    @xkcd.group(invoke_without_subcommand=False)
+    async def explain(self, ctx, *, xkcd_id: Union[int, str] = 221):
+        """links to an xkcd explanation"""
+        try:
+            data = await self.get_xkcd(xkcd_id)
+        except ContentTypeError as e:
+            await ctx.send(f"`ContentTypeError: {e}`")
+            await ctx.send(f"was your input valid?")
+            return
+        link = f"https://explainxkcd.com/{data['num']}"
+        await ctx.send(link)
+
+    @explain.command()
+    async def latest(self, ctx):
+        """links to the latest xkcd explanation"""
+        data = await self.get_xkcd(xkcd_id=None)
+        link = f"https://explainxkcd.com/{data['num']}"
+        await ctx.send(link)
+
+    @explain.command()
+    async def random(self, ctx):
+        """links to random xkcd explanation"""
+        data = await self.get_random_xkcd()
+        link = f"https://explainxkcd.com/{data['num']}"
+        await ctx.send(link)
 
 
 def setup(bot):
