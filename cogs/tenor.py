0bf8a429875677529f788cdf7392264a85eee166
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-05-08 19:51:49 -0400

    Added tenor.py

---
+++ cogs/tenor.py
@@ -0,0 +1,26 @@
+import discord.ext.commands as commands
+from aiohttp import ClientSession
+
+from configs.secrets import tenor_api_key as apikey
+
+
+class Tenor(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @staticmethod
+    async def get_tenor(query) -> str:
+        async with ClientSession() as session:
+            async with session.get(f"https://g.tenor.com/v1/search?q={query}&key={apikey}&limit=1") as resp:
+                json = await resp.json()
+                if not isinstance(json, dict):
+                    return "What the fuck"
+                return json['results'][0]['url']
+
+    @commands.command(aliases=["t"])
+    async def tenor(self, ctx, *, query):
+        await ctx.send(await self.get_tenor(query))
+
+
+def setup(bot):
+    bot.add_cog(Tenor(bot))
