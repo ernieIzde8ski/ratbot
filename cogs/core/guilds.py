99d64716975b8186a26b2bb30fd3c82b378b3870
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-05-15 17:01:56 -0400

    Allow list_guilds command to sort by several methods

--- cogs/core/guilds.py
+++ cogs/core/guilds.py
@@ -1,3 +1,5 @@
+from typing import Optional, Union
+
 import discord.ext.commands as commands
 from discord import Color, Embed, Guild
 
@@ -5,6 +7,11 @@ from discord import Color, Embed, Guild
 class GuildUpdate(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.sorts = {
+            "alphabetical": ["alphabetical", "alphabetic", "a-z", "az"],
+            "length": ["lengths", "length", "len"],
+            "total_members": ["total_members", "member_list", "members"]
+        }
 
     async def embed_constructor(self, color: Color, guild: Guild, action: str) -> Embed:
         print(self.bot.user)
@@ -27,12 +34,36 @@ class GuildUpdate(commands.Cog):
         embed = await self.embed_constructor(Color.red(), guild, "Left")
         await self.bot.config.channels.guilds.send(embed=embed)
 
-    @commands.command(hidden=True)
+    def sort_guilds(self, guilds: list, sort_method: str = "alphabetical", sort_order: Optional[bool] = False) -> Union[
+        list, bool]:
+        sort_method = sort_method.lower().replace(" ", "_")
+        if sort_method in self.sorts['alphabetical']:
+            guilds.sort(key=lambda g: g.name, reverse=sort_order)
+        elif sort_method in self.sorts['length']:
+            guilds.sort(key=lambda g: len(g.name), reverse=sort_order)
+        elif sort_method in self.sorts['total_members']:
+            guilds.sort(key=lambda g: g.member_count, reverse=sort_order)
+        else:
+            return False
+        return guilds
+
+    @commands.command(aliases=["guilds", "list_servers", "servers"])
     @commands.is_owner()
-    async def list_guilds(self, ctx):
-        guilds = ""
-        for guild in self.bot.guilds:
-            guilds += f"{guild}\n"
+    async def list_guilds(self, ctx, sort_order: Optional[bool] = False, *, sort_method: Optional[str] = "az"):
+        """Print list of guilds the bot is a member of
+        Parameters:
+            sort_order: Reverse the sort order
+            sort_method: Self-explanatory
+        Valid sort_methods:
+            "alphabetical": ["alphabetical", "alphabetic", "a-z", "az"],
+            "length": ["lengths", "length", "len"],
+            "total_members": ["total_members", "member_list", "members"]"""
+        guilds = self.bot.guilds
+        if sort_method:
+            guilds = self.sort_guilds(guilds, sort_method, sort_order)
+            if not guilds: return await ctx.send("This is not a valid sort method")
+        guilds = "\n".join([f"{guild} `({guild.member_count})`" for guild in guilds])
+
         embed = Embed(description=guilds).set_footer(text=f"total guilds: {len(self.bot.guilds)}")
         await ctx.send(embed=embed)
 
