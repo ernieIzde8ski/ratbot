8040acb058595d75f1ef0aa3f9c4e680f504aafa
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-05-21 17:55:08 -0400

    Add analysis of wind speed to Armenium.py

--- cogs/on_member_update/Armenium.py
+++ cogs/on_member_update/Armenium.py
@@ -13,14 +13,28 @@ from pytz import timezone
 def match_temp(temperature: float) -> str:
     temperatures = (
         [-40, "siberia"], [-30, "sub-30"], [-20, "sub-20"], [-15, "sub-15"], [-10, "sub-10"], [-5, "sub5"], [0, "sub0"],
-        [5, "sub5"],
-        [10, "sub10"], [15, "sub15"], [20, "sub20"], [25, "sub25"], [30, "sub30"], [35, "sub35"], [40, "sub40"]
+        [5, "sub5"], [10, "sub10"], [15, "sub15"], [20, "sub20"], [25, "sub25"], [30, "sub30"], [35, "sub35"],
+        [40, "sub40"]
     )
     for temp, name in temperatures:
         if temperature <= temp:
             return name
     else:
         return "post40"
+
+
+def match_wind(speed: float) -> str:
+    winds = (
+        [0.45, "Calm"], [1.34, "Light Air"], [3.13, "Light Breeze"], [5.36, "Gentle Breeze"], [8.05, "Moderate Breeze"],
+        [10.73, "Fresh Breeze"], [13.86, "Strong Breeze"], [16.99, "Near Gale"], [20.56, "Gale"],
+        [24.14, "Severe Gale"], [28.16, "Storm"], [32.19, "Violent Storm"], [42.49, "Cat. 1 Hurricane"],
+        [49.17, "Cat. 2 Hurricane"], [57.67, "Cat. 3 Hurricane"], [69.74, "Cat. 4 Hurricane"]
+    )
+    for windspeed, name in winds:
+        if speed <= windspeed:
+            return name
+    else:
+        return "Cat. 5 Hurricane"
 
 
 class Armenium(commands.Cog):
@@ -54,7 +68,8 @@ class Armenium(commands.Cog):
         nick = random.choice(self.data[auth_id]['nicknames'])
         temperature = f"It is currently {temps['true']} degrees Celsius outside"
         if temps['true'] != temps['felt']: temperature += f" (and it feels like {temps['felt']})"
-        weather = f"The Wind is going at {temps['wind']} Meter per second & the weather is `{temps['weather']}`"
+        weather = f"The Wind is going at {temps['wind']} Meter per second ({match_wind(temps['wind'])}) " \
+                  f"& the weather is `{temps['weather']}`"
         temperature_analysis = self.msg['temps'][match_temp(temps['felt'])]
         russian = " ".join(f'{sentence} ' for sentence in random.sample(self.msg['russian'], random.randint(3, 5)))
 
