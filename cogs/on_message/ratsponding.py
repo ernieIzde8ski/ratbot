4bf5d6eb2f1e93c93c7ae43dacf660679f8f455e
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-05-25 04:49:38 -0400

    Trolled

--- cogs/on_message/ratsponding.py
+++ cogs/on_message/ratsponding.py
@@ -5,8 +5,8 @@ class Ratsponding(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
+    @commands.Cog.listener("on_message")
+    async def rat(self, message):
         if message.guild:
             # prune non rat messages in #rat channels
             if message.channel.name == "rat" and message.content != "rat":
@@ -20,6 +20,11 @@ class Ratsponding(commands.Cog):
             log = f"[{self.bot.static.now()}] rat from {message.author} in "
             log += "dms" if not message.guild else str(message.guild)
             print(log)
+
+    @commands.Cog.listener("on_message")
+    async def trolfl(self, message):
+        if message.author.bot or not message.content: return
+        if "troll" in message.content.lower(): await message.add_reaction("<:trolled:846670001691557938>")
 
 
 def setup(bot):
