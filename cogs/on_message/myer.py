a73d993bd3dce52c03e0b28e1314c7ba8ae16b5a
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-01 16:33:28 -0400

    Generalize myer.py

--- cogs/on_message/myer.py
+++ cogs/on_message/myer.py
@@ -15,30 +15,36 @@ class Myer(commands.Cog):
         """Generate response"""
         # actually generate response
         if str(self.data['responses_given']) in list(self.data['responses']):
-            response = self.data['responses'][str(self.data['responses_given'])]
+            response = self.data['responses'][str(
+                self.data['responses_given'])]
         else:
             response = choice(self.data['responses']['generic'])
         # return response
         return response
 
     async def respond(self, msg):
-        if not msg.guild: return
-        if msg.guild.id != 488475203303768065: return
+        if not msg.guild:
+            return
+        if msg.guild.id != 488475203303768065:
+            return
         # check if the guild is 0413-Theta
 
         # check for twitter links from myer
-        if "https://twitter.com" in msg.content and msg.author.id == self.data['myer']:
-            # Punishment.
-            try:
-                await msg.delete()
-            except Forbidden:
-                return await msg.channel.send("Hit me, nail me, make me God !!!!!!!!!!!")
-            await msg.channel.send(
-                f"{msg.author.mention} {self.response()}", allowed_mentions=AllowedMentions(users=True)
-            )
-            with open("cogs/on_message/myer.json", "w", encoding="utf-8") as f:
-                self.data["responses_given"] += 1
-                json.dump(self.data, f)
+        for item in self.data['blocked']:
+            if item in msg.content and msg.author.id == self.data['myer']:
+                # Punishment.
+                try: await msg.delete()
+                except Forbidden:
+                    return await msg.channel.send("Hit me, nail me, make me God !!!!!!!!!!!")
+                await msg.channel.send(
+                    f"{msg.author.mention} {self.response()}", allowed_mentions=AllowedMentions(users=True)
+                )
+
+                with open("cogs/on_message/myer.json", "w", encoding="utf-8") as f:
+                    self.data["responses_given"] += 1
+                    json.dump(self.data, f)
+
+                return
 
     @commands.Cog.listener()
     async def on_message(self, message):
