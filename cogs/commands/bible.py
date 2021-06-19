754a7d3ca7b0a3b1366bf673d0c3a3aa185ba5ef
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-19 01:11:10 -0400

    Allow setting bible translations

--- cogs/commands/bible.py
+++ cogs/commands/bible.py
@@ -3,11 +3,16 @@ from aiohttp import ClientSession
 from discord.ext import commands
 from json import load, dump
 from discord import Embed
+from typing import Optional
 
 
 class Bible(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.translation_languages = {"cherokee": ["Cherokee New Testament", "Cherokee"], "kjv": ["King James Version", "English"],
+                                      "web": ["World English Bible", "English"], "clementine": ["Clementine Latin Vulgate", "Latine"],
+                                      "almeida": ["João Ferreira de Almeida", "Português"], "rccv": ["Romanian Corrected Cornilescu Bible","Română"]}
+        self.valid_translations = self.translation_languages.keys()
         try:
             with open("data/bible.json", "r") as file:
                 self.data = load(file)
@@ -17,9 +22,9 @@ class Bible(commands.Cog):
                 dump(self.data, file)
 
     @staticmethod
-    async def get_text(reference, characters_per_line: int = 70) -> dict:
+    async def get_text(reference, translation, characters_per_line: int = 70) -> dict:
         async with ClientSession() as session:
-            async with session.get(f"https://bible-api.com/{reference}") as resp:
+            async with session.get(f"https://bible-api.com/{reference}?translation={translation}") as resp:
                 resp = await resp.json()
 
                 if resp.get("error"):
@@ -34,19 +39,29 @@ class Bible(commands.Cog):
                 }
 
     @staticmethod
-    def embed_constructor(text: dict, color) -> Embed:
+    def embed_constructor(text: dict, color, display_footer_hint: bool) -> Embed:
         url = "https://biblegateway.com/passage/?search={}&version=NIV"
+        footer = f"Translation: {text['translation']}"
+        if display_footer_hint == True:
+            footer += " | If this version is difficult or this message too annoying, you can click the link or use the set_translation command"
         return Embed(
             title=text["reference"],
             description=text["content"],
             color=color,
             url=url.format(text["reference"].replace(" ", "%20"))
-        ).set_footer(text=f"Translation: {text['translation']}")
+        ).set_footer(text=footer)
 
     @commands.command(aliases=["v", "🙏"])
     async def bible_verse(self, ctx, *, reference: str = "Joshua 21:8"):
         """Returns a passage based off a reference"""
-        text = await self.get_text(reference)
+        if self.data.get(str(ctx.author.id)):
+            translation = self.data[str(ctx.author.id)]
+            translation_not_set = False
+        else:
+            translation = "KJV"
+            translation_not_set = True
+
+        text = await self.get_text(reference, translation)
 
         if text.get("error"):
             return await ctx.send(f"error: {text['error']}")
@@ -54,7 +69,28 @@ class Bible(commands.Cog):
             reference = text["reference"].replace(' ', "%20")
             return await ctx.send(f"error: passage is too long\ntry https://biblegateway.com/passage/?search={reference}")
         else:
-            await ctx.send(embed=self.embed_constructor(text, ctx.me.color))
+            await ctx.send(embed=self.embed_constructor(text, ctx.me.color, translation_not_set))
+
+    @commands.command(aliases=["set_version"])
+    async def set_translation(self, ctx, *, translation: Optional[str]):
+        """Sets a version to use in the bible_verse command
+        Due to the limitations of the api I use (https://bible-api.com/),
+        there's only a few functional translations (displayed by invoking
+        the command)."""
+        if translation:
+            translation = translation.lower()
+        if not translation:
+            await ctx.send(f"Available translations: `{'`, `'.join(self.valid_translations)}`")
+        elif translation not in self.valid_translations:
+            await ctx.send(
+                f"Invalid translation!\n"
+                f"Available translations: `{'`, `'.join(self.valid_translations)}`"
+            )
+        else:
+            self.data[str(ctx.author.id)] = translation
+            with open("data/bible.json", "w") as file:
+                dump(self.data, file)
+            await ctx.send(f"Set your translation to `{self.translation_languages[translation][0]}` ({self.translation_languages[translation][1]})")
 
 
 def setup(bot):
