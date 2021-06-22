6872e444d28315ea2924e9909b182fe2691fe636
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-21 23:42:11 -0400

    Use safe_load and safe_dump in prefixes module

--- modules/prefixes.py
+++ modules/prefixes.py
@@ -1,16 +1,11 @@
+from modules.json import safe_dump, safe_load
 from json import load, dump
 from discord.ext.commands import when_mentioned, when_mentioned_or
 
 class Prefixes:
     def __init__(self, default_prefix):
         self.prefix = default_prefix
-        try:
-            with open("data/prefixes.json", "r", encoding="utf-8") as file:
-                self.prefixes = load(file)
-        except FileNotFoundError:
-            self.prefixes = {}
-            with open("data/prefixes.json", "x") as file:
-                dump(self.prefixes, file)
+        self.prefix = safe_load("data/prefixes.json", {})
 
     async def get_prefix(self, bot, message):
         if not message.guild:
@@ -23,5 +18,4 @@ class Prefixes:
     
     async def update_prefixes(self, id, new_prefix):
         self.prefixes[id] = new_prefix
-        with open("data/prefixes.json", "w", encoding="utf-8") as file:
-            dump(self.prefixes, file)
+        safe_dump("data/prefixes.json", self.prefixes)
