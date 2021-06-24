ebd8ce4207da2732cbaeb363870f275598e88fe9
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-24 02:15:59 -0400

    Add die command to terminate client

--- cogs/on_ready/log.py
+++ cogs/on_ready/log.py
@@ -7,20 +7,29 @@ from modules.json import safe_load
 class Log(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.emoji = safe_load("data/emoji.json", "🎃")
+        self.emoji = safe_load("data/emoji.json", ["🐣", "🎃"])
 
     def get_channels(self):
         if not self.bot.channels.loaded:
             self.bot.channels.get_channels(self.bot)
 
-    def embed_constructor(self):
-        return Embed(title=f"{self.emoji} Online!", color=Color.dark_green())
+    def embed_constructor(self, status: str):
+        if status == "online":
+            return Embed(title=f"{self.emoji[0]} Online!", color=Color.dark_green())
+        elif status == "offline":
+            return Embed(title=f"{self.emoji[1]} Offline!", color=Color.from_rgb(91, 10, 0))
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.get_channels()
         print(f"{self.bot.user.name}#{self.bot.user.discriminator} online!")
-        await self.bot.channels.k["Status"].send(embed = self.embed_constructor())
+        await self.bot.channels.k["Status"].send(embed = self.embed_constructor("online"))
+    
+    @commands.command()
+    async def die(self, ctx):
+        self.get_channels()
+        await self.bot.channels.k["Status"].send(embed = self.embed_constructor("offline"))
+        await self.bot.close()
 
 
 def setup(bot):
