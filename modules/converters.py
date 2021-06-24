f1ecd7f60d3a8dccc622e6c4b11e26d8258decb2
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-23 21:57:05 -0400

    Make flag converter more like terminal

--- modules/converters.py
+++ modules/converters.py
@@ -1,45 +1,32 @@
 from discord.ext import commands
+from re import split
 from json import loads
 from json.decoder import JSONDecodeError
 
+
 class FlagConverter(commands.Converter):
-    async def convert(self, ctx: commands.Context, argument: str) -> dict:
-        """Converter largely following a discord.py model Lol"""
-        # Initialize dict, arguments values
+    async def convert(self, ctx: commands.Context, arguments: str) -> dict:
+        if not arguments.startswith("--"):
+            raise commands.BadArgument("Arguments must begin with --")
         dict_ = {}
-        arguments = argument.split()
-        if not arguments:
-            raise commands.BadArgument("No arguments exist")
+        arguments = arguments.replace(";", " ")
+        arguments = split(r"\s+--", arguments[2:])
+        for argument in arguments:
+            if not argument:
+                raise commands.BadArgument("Argument key must exist")
 
-        for argument in iter(arguments):
-            if not argument.startswith(("–", "--")):
-                raise commands.BadArgument("Argument must begin with -- or –")
-            
-            # split argument if a value is specified with = or :
-            argument = argument.replace("=", ":").split(":")
-            if not argument[0]:
-                raise commands.BadArgument("Argument value must exist")
+            value = argument.split()
+            key = value.pop(0)
 
-            # raise an error if more than one value is passed, or set the value to True if no value is passed
-            length = argument.__len__()
-            if length > 2:
-                raise commands.BadArgument("Argument cannot have more than one value")
-            elif length == 1:
-                argument.append(True)
+            if not value:
+                value = True
+            else:
+                value = " ".join([i.replace("__", " ") for i in value])
+                try:
+                    value = loads(value)
+                except JSONDecodeError:
+                    pass
 
-            # try and convert the value to another type like int or something
-            try:
-                argument[1] = loads(argument[1].lower())
-            except (JSONDecodeError, AttributeError):
-                pass
-            
-            # replace underscores with spaces in string arguments
-            if isinstance(argument[1], str):
-                argument[1] = argument[1].replace("_", " ")
+            dict_[key] = value
 
-            # clean up argument name
-            argument[0] = argument[0].removeprefix("--").removeprefix("–").lower()
-
-            # add flag to dictionary
-            dict_[argument[0].lower()] = argument[1]
         return dict_
