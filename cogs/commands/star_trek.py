5e80bfa39b9b2d38bc6651a2f1949b3f223e137b
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-25 22:54:36 -0400

    Reformat star_trek.py

--- cogs/commands/star_trek.py
+++ cogs/commands/star_trek.py
@@ -8,12 +8,12 @@ class Trek(commands.Cog):
     def __init__(self):
         self.tos_generator = loadGenerator("modules/Star Trek.meow")
         self.borg_samples = safe_load("modules/borg.json", [])
-        
-    
+
     @commands.command()
-    async def borg(self, ctx, *, borg: str="Borg"):
+    async def borg(self, ctx, *, borg: str = "Borg"):
         resp = "We are the {Borg}. "
-        resp += " ".join(random.sample(self.borg_samples, k=random.randint(2, len(self.borg_samples))))
+        resp += " ".join(random.sample(self.borg_samples,
+                         k=random.randint(2, len(self.borg_samples))))
         await ctx.send(resp.format(Borg=borg, Locutus=ctx.author.display_name.title()))
 
     @commands.command(aliases=["rt", "trek"])
