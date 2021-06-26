4cd8e9b6637b67676bc6058186e637e0c77a66de
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-26 00:57:40 -0400

    Commit xkcd cache generator

---
+++ modules/xkcd_cache_generator.js
@@ -0,0 +1,56 @@
+const fetch = require("node-fetch");
+const fs = require('fs');
+const range = require('./javascript/range');
+
+var data = [];
+
+get_xkcd = async int => {
+    if (int == -1) {
+        url = "https://xkcd.com/info.0.json";
+    } else {
+        url = `https://xkcd.com/${int}/info.0.json`;
+    }
+
+    resp = await fetch(url);
+    try {
+        resp = await resp.json();
+    } catch (e) {
+        resp = {
+            error: e
+        };
+    }
+    return resp;
+};
+
+get_xkcds = async int => {
+    var list = [];
+    var latest = await get_xkcd(-1);
+    var xkcds = range(stop = latest.num + 1);
+
+    for (var xkcd of xkcds) {
+        xkcd = await get_xkcd(xkcd);
+        if (xkcd.error) continue;
+        list.push(xkcd);
+        console.log(xkcd.num, xkcd.title);
+    }
+    return list.map(item => {
+        return {
+            name: item.title,
+            alt: item.alt,
+            int: item.num
+
+        };
+    });
+};
+
+if (require.main === module) {
+    console.log('called directly');
+
+    get_xkcds()
+        .then(list => {
+            console.log(list);
+            list = JSON.stringify(list);
+            fs.writeFile("./xkcd_cache.json", list, callback => "");
+        });
+
+}
