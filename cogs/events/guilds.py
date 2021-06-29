64c2c0b71c94936d7e57496f666e92bfaa9151d9
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-29 15:12:47 -0400

    Commit guilds.py

---
+++ cogs/events/guilds.py
@@ -0,0 +1,53 @@
+from typing import Optional
+from modules.converters import FlagConverter
+from discord import Guild, Embed, Color
+from discord.ext import commands
+
+
+class Guilds(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild: Guild):
+        embed = Embed(
+            title=f"Joined guild: {guild}",
+            description=f"Members: {guild.member_count}\nID: {guild.id}",
+            color=Color.dark_green()
+        ).set_thumbnail(url=guild.icon_url)
+        await self.bot.c.Guilds.send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild: Guild):
+        embed = Embed(
+            title=f"Left guild: {guild}",
+            description=f"ID: {guild.id}",
+            color=Color.dark_red()
+        ).set_thumbnail(url=guild.icon_url)
+        await self.bot.c.Guilds.send(embed=embed)
+
+    @commands.command()
+    @commands.is_owner()
+    async def guilds(self, ctx: commands.Context, *, sort: FlagConverter = {}):
+        """
+        Get guild list
+        Usage:
+            r;guilds
+            r;guilds --order member_count --reverse
+        """
+        order = "alphabetical" if not sort.get("order") else str(sort.get("order")).lower()
+        reverse = bool(sort.get("reverse"))
+
+        if order == "alphabetical":
+            guilds = sorted(self.bot.guilds, key=lambda guild: guild.name.lower(), reverse=reverse)
+            guilds = map(lambda guild: f"{guild}", guilds)
+        elif order == "member_count":
+            guilds = sorted(self.bot.guilds, key=lambda guild: guild.member_count, reverse=reverse)
+            guilds = map(lambda guild: f"{guild} ({guild.member_count} members)", guilds)
+        else:
+            return await ctx.send("Invalid sort option")
+        await ctx.send("\n".join(guilds))
+
+
+def setup(bot):
+    bot.add_cog(Guilds(bot))
