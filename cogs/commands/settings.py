dbb365037d9b1b57e2d6753beae8af394e9fdea1
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-29 21:25:15 -0400

    Move toggle_random_bands command to settings.py

--- cogs/commands/settings.py
+++ cogs/commands/settings.py
@@ -1,12 +1,14 @@
 from discord.ext import commands
-from typing import Optional
 from modules._json import safe_load, safe_dump
+from modules.converters import Percentage
+from typing import Optional
 
 
 class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.bot.tenor_guilds = set(safe_load("data/tenor_guilds.json", []))
+        self.bot.banning_guilds = safe_load("data/banning.json", {})
 
     @commands.command(aliases=["prefix"])
     @commands.has_permissions(manage_guild=True)
@@ -32,6 +34,25 @@ class Settings(commands.Cog):
             await ctx.send("Enabling tenor slaying in this server")
             self.bot.tenor_guilds.add(ctx.guild.id)
         safe_dump("data/tenor_guilds.json", list(self.bot.tenor_guilds))
+    
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def toggle_random_bans(self, ctx, percent: Optional[Percentage]):
+        if not ctx.guild:
+            return await ctx.send("You need to be in a guild for this!")
+
+        id = str(ctx.guild.id)
+        if id in self.bot.banning_guilds and not percent:
+            await ctx.send("Disabling random bans in this guild")
+            self.bot.banning_guilds.pop(id)
+        else:
+            if not percent:
+                percent = 0.00002
+            elif not (0 < percent < 1):
+                return await ctx.send("Error: Percentage must be from 0% to 100% (or 0.0 to 1.0)")
+            await ctx.send(f"Enabling random bans in this guild with a {percent * 100}% chance")
+            self.bot.banning_guilds[id] = percent
+        safe_dump("data/banning.json", self.bot.banning_guilds)
 
 
 def setup(bot):
--- cogs/on_message/random_bans.py
+++ cogs/on_message/random_bans.py
@@ -2,7 +2,6 @@ from discord.ext import commands
 from discord import Message, AllowedMentions, Forbidden
 from asyncio import sleep
 from modules._json import safe_load, safe_dump
-from modules.converters import Percentage
 from typing import Optional
 from random import random
 
@@ -10,26 +9,6 @@ from random import random
 class Bans(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.guilds = safe_load("data/banning.json", {})
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def toggle_random_bans(self, ctx, percent: Optional[Percentage]):
-        if not ctx.guild:
-            return await ctx.send("You need to be in a guild for this!")
-
-        id = str(ctx.guild.id)
-        if id in self.guilds and not percent:
-            await ctx.send("Disabling random bans in this guild")
-            self.guilds.pop(id)
-        else:
-            if not percent:
-                percent = 0.00002
-            elif not (0 < percent < 1):
-                return await ctx.send("Error: Percentage must be from 0% to 100% (or 0.0 to 1.0)")
-            await ctx.send(f"Enabling random bans in this guild with a {percent * 100}% chance")
-            self.guilds[id] = percent
-        safe_dump("data/banning.json", self.guilds)
 
     @commands.Cog.listener()
     async def on_message(self, message: Message):
@@ -37,9 +16,9 @@ class Bans(commands.Cog):
             return
 
         guild_id = str(message.guild.id)
-        if guild_id not in self.guilds:
+        if guild_id not in self.bot.banning_guilds:
             return
-        elif random() > self.guilds[guild_id]:
+        elif random() > self.bot.banning_guilds[guild_id]:
             return
 
         message = await message.reply("Uh Oh", allowed_mentions=AllowedMentions.all())
