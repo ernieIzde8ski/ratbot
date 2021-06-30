19e6efcf86a86160e37e273ca8d55b5ccc9f580a
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-06-29 20:11:14 -0400

    Create a togglable Tenor gif deleter

--- cogs/commands/settings.py
+++ cogs/commands/settings.py
@@ -1,10 +1,12 @@
-from typing import Optional
 from discord.ext import commands
+from typing import Optional
+from modules._json import safe_load, safe_dump
 
 
 class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bot.tenor_guilds = set(safe_load("data/tenor_guilds.json", []))
 
     @commands.command(aliases=["prefix"])
     @commands.has_permissions(manage_guild=True)
@@ -16,6 +18,20 @@ class Settings(commands.Cog):
         self.bot.dispatch("prefix_update", str(ctx.guild.id), prefix)
         prefix = prefix.replace("`", "\`")
         await ctx.send(f"Updated prefix to {prefix}")
+
+    @commands.command(aliases=["toggle_tenor"])
+    @commands.has_permissions(manage_guild=True)
+    async def tenor_toggle(self, ctx):
+        if not ctx.guild:
+            return await ctx.send("You must be in a guild to run this command!")
+
+        if ctx.guild.id in self.bot.tenor_guilds:
+            await ctx.send("Disabling tenor slaying in this server")
+            self.bot.tenor_guilds.remove(ctx.guild.id)
+        else:
+            await ctx.send("Enabling tenor slaying in this server")
+            self.bot.tenor_guilds.add(ctx.guild.id)
+        safe_dump("data/tenor_guilds.json", list(self.bot.tenor_guilds))
 
 
 def setup(bot):
