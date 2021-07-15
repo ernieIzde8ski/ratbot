36fa1c3113d105b10bbef96ff6457f89385868e2
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-07-14 22:10:02 -0400

    Handle CommandNotFound error better

--- cogs/events/error_handling.py
+++ cogs/events/error_handling.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from fuzzywuzzy import fuzz
 
 
 class ErrorHandling(commands.Cog):
@@ -9,7 +10,13 @@ class ErrorHandling(commands.Cog):
     async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
         if ctx.prefix == "" and isinstance(error, commands.CommandNotFound):
             return
-        await ctx.reply(f"{error.__class__.__name__}: {error}")
+        
+        resp = f"{error.__class__.__name__}: {str(error).removesuffix('.')}.\n"
+        if isinstance(error, commands.CommandNotFound):
+            cmds = list(map(lambda cmd: cmd.name.lower(), ctx.bot.commands))
+            cmds.sort(key=lambda cmd: fuzz.ratio(ctx.invoked_with.lower(), cmd), reverse=True)
+            resp += f"Closest match: `{cmds[0]}`."
+        await ctx.reply(resp)
 
 
 def setup(bot):
