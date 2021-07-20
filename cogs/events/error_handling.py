bbff23739df56c293941bd564913ba2f1a7bb2f7
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-07-20 00:14:08 -0400

    Include aliases in CommandNotFound error handling

--- cogs/events/error_handling.py
+++ cogs/events/error_handling.py
@@ -15,8 +15,13 @@ class ErrorHandling(commands.Cog):
                 return
         
         resp = f"{error.__class__.__name__}: {error}\n"
+        # Show the closest matching command if the error is CommandNotFound.
         if isinstance(error, commands.CommandNotFound):
+            # Generate command name list, with aliases included.
             cmds = list(map(lambda cmd: cmd.name.lower(), ctx.bot.commands))
+            aliases = list(map(lambda cmd: cmd.aliases, ctx.bot.commands))
+            aliases = [item for elem in aliases for item in elem]
+            cmds += aliases
             cmds.sort(key=lambda cmd: fuzz.ratio(ctx.invoked_with.lower(), cmd), reverse=True)
             resp += f"Closest match: `{cmds[0]}`."
         await ctx.reply(resp)
