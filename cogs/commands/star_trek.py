6a65e336b5e619b5f2fada8838f2ba1c5a264280
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-08-13 00:49:59 -0400

    Update borg command

--- cogs/commands/star_trek.py
+++ cogs/commands/star_trek.py
@@ -9,14 +9,13 @@ class Trek(commands.Cog):
     def __init__(self):
         self.tos_generator = loadGenerator("modules/JSON/Star_Trek.meow")
         self.borg_samples = safe_load("modules/JSON/borg.json", [])
+        self.borglen = len(self.borg_samples)
 
     @commands.command()
     async def borg(self, ctx, *, borg: str = "Borg"):
         """Sends a Borg copypasta with an argument as Borg"""
-        resp = "We are the {Borg}. "
-        resp += " ".join(random.sample(self.borg_samples,
-                                       k=random.randint(2, len(self.borg_samples))
-        ))
+        resp = random.choices(["We are the {Borg}. ", "I am {Locutus} of {Borg}. "], [5, 3], k=1)[0]
+        resp += " ".join(random.sample(self.borg_samples, k=random.randint(2, self.borglen)))
         await ctx.send(resp.format(Borg=borg, Locutus=ctx.author.display_name.title()))
 
     @commands.command(aliases=["rt", "trek"])
