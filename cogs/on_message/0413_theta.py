948bd29ca5794269ac722cf5a4e08bd061a0a0bb
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2021-09-06 13:53:02 -0400

    Fix armenium counter bug

--- cogs/on_message/0413_theta.py
+++ cogs/on_message/0413_theta.py
@@ -8,13 +8,17 @@ class AEBDTheta(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.armenium_facts: int = safe_load("data/facts.json", 0)
-        self.channel: discord.TextChannel = self.bot.get_channel(
-            884237172717277225)
+        self.bot.loop.create_task(self.set_channel())
+
+    async def set_channel(self):
+        await self.bot.wait_until_ready()
+        self.channel: discord.TextChannel = self.bot.get_channel(884237172717277225)
 
     @commands.command()
     @commands.is_owner()
     async def set_facts(self, ctx: commands.Context, arg: int):
-        await self.channel.edit(topic=f"**Armenium is intimidated by **Men**.\nKnown Armenium Facts: {self.armenium_facts := self.armenium_facts + 1}")
+        self.armenium_facts += 1
+        await self.channel.edit(topic=f"**Armenium is intimidated by **Men**.\nKnown **Armenium** Facts: {self.armenium_facts}")
         safe_dump("data/facts.json", self.armenium_facts)
         await ctx.send(f"Set armenium facts to `{self.armenium_facts}`")
 
@@ -23,7 +27,8 @@ class AEBDTheta(commands.Cog):
         if message.author.id == 304118384663068673 and message.channel.id == self.channel.id:
             if fuzz.ratio(message.content.lower(), "**daily 'armenium' fact**") > 85:
                 return
-            await self.channel.edit(topic=f"**Armenium is intimidated by **Men**.\nKnown Armenium Facts: {self.armenium_facts := self.armenium_facts + 1}")
+            self.armenium_facts += 1
+            await self.channel.edit(topic=f"**Armenium is intimidated by **Men**.\nKnown **Armenium** Facts: {self.armenium_facts}")
 
 
 def setup(bot: commands.Bot) -> None:
