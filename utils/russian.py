c720098d11248e945aa2c4d6ab65c9e0ed9104aa
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-01-08 05:40:16 -0500

    Remove unused functions in russian.py

--- utils/russian.py
+++ utils/russian.py
@@ -1,11 +1,7 @@
-import json
-from pathlib import Path
 from typing import TypedDict
 import random
 
 
-# Before I totally deprecate this, might as well make it absolute so my issues go away
-PATH = (Path(__file__).parent / Path("../data/russian.json")).resolve()
 URL = "https://raw.githubusercontent.com/thiagobodruk/bible/master/json/ru_synodal.json"
 
 
@@ -21,11 +17,6 @@ class BibleBook(TypedDict):
 Bible = list[BibleBook]
 
 
-def load() -> Bible:
-    with open(PATH, "r", encoding="utf-8") as file:
-        return json.load(file)
-
-
 def converter(bible: Bible) -> list[str]:
     resp = []
     for book in bible:
@@ -35,27 +26,3 @@ def converter(bible: Bible) -> list[str]:
     # I'm not really gonna need the *whole* bible.
     resp = random.sample(resp, 10000)
     return resp
-
-
-def _main(bible: Bible = None) -> None:
-    """Function called by main. Exists so that _main might use exception handling."""
-    resp = converter(bible or load())
-    with open(PATH, "w+", encoding="utf-8") as file:
-        json.dump(resp, file)
-    print("Converted Russian bible into proper format!")
-
-
-def main(bible: Bible = None):
-    try:
-        _main()
-    except TypeError:
-        con = input("Bible has already been processed. Retry? [y/N] ")
-        if con and con[0] in ("y", "Y", "1"):
-            import requests
-
-            bible = json.loads(requests.get(URL).content)
-            _main(bible)
-
-
-if __name__ == "__main__":
-    main()
