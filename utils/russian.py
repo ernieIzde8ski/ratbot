53d79fd4aba6bd42877b469571255881c5e01e24
ernieIzde8ski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-01-08 03:12:59 -0500

    Turn russian.py into functions

--- utils/russian.py
+++ utils/russian.py
@@ -1,19 +1,61 @@
 import json
 from pathlib import Path
+from typing import TypedDict
+import random
 
-path = Path("../data/russian.json")
 
-with open(path, "r", encoding="utf-8-sig") as file:
-    data = json.load(file)
-    verses = []
-    for book in data:
+# Before I totally deprecate this, might as well make it absolute so my issues go away
+PATH = (Path(__file__).parent / Path("../data/russian.json")).resolve()
+URL = "https://raw.githubusercontent.com/thiagobodruk/bible/master/json/ru_synodal.json"
+
+
+BibleVerse = list[str]
+
+
+class BibleBook(TypedDict):
+    abbrev: str
+    chapters: list[BibleVerse]
+    name: str
+
+
+Bible = list[BibleBook]
+
+
+def load() -> Bible:
+    with open(PATH, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def converter(bible: Bible) -> list[str]:
+    resp = []
+    for book in bible:
         for chapter in book["chapters"]:
             for verse in chapter:
-                verses.append(verse)
-        # prevent the bible from getting *too* large
-        if len(verses) > 10000:
-            break
+                resp.append(verse)
+    # I'm not really gonna need the *whole* bible.
+    resp = random.sample(resp, 10000)
+    return resp
 
-with open(path, "w+", encoding="utf-8") as file:
-    json.dump(verses, file)
-    print("Converted Russian bible to proper format")
+
+def _main(bible: Bible = None) -> None:
+    """Function called by main. Exists so that _main might use exception handling."""
+    resp = converter(bible or load())
+    with open(PATH, "w+", encoding="utf-8") as file:
+        json.dump(resp, file)
+    print("Converted Russian bible into proper format!")
+
+
+def main(bible: Bible = None):
+    try:
+        _main()
+    except TypeError:
+        con = input("Bible has already been processed. Retry? [y/N] ")
+        if con and con[0] in ("y", "Y", "1"):
+            import requests
+
+            bible = json.loads(requests.get(URL).content)
+            _main(bible)
+
+
+if __name__ == "__main__":
+    main()
