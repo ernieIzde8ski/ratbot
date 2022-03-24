22423295d91199aa359808138b008fc773e8021b
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2022-03-24 13:48:07 -0400

    Allow using None as default for safe_load

--- utils/functions.py
+++ utils/functions.py
@@ -20,13 +20,13 @@ def strip_str(text: str) -> str:
     return resp
 
 
-def safe_load(fp: str, backup: Any = None) -> Any:
+def safe_load(fp: str, backup: Any = ...) -> Any:
     """Load a file & create it from the backup variable if it doesn't exist"""
     try:
         with open(fp, "r", encoding="utf-8") as file:
             return json.load(file)
     except FileNotFoundError as err:
-        if backup is None:
+        if backup is ...:
             raise err from err
         with open(fp, "x", encoding="utf-8") as file:
             json.dump(backup, file)
