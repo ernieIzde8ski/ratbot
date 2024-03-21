61dc01be793682015a72d5cc78e52220601f636e
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-03-21 19:43:06 -0400

    Add cog.cogs, extension loading management

--- cogs/cogs.py
+++ cogs/cogs.py
@@ -1,7 +1,17 @@
 import logging
 import traceback
+from typing import Callable
 
-from lib.bot import Bot, Cog, CommandError, Interaction, slash_command
+from disnake.ext import commands
+from disnake.ext.commands import (
+    ExtensionAlreadyLoaded,
+    ExtensionNotFound,
+    ExtensionNotLoaded,
+    NotOwner,
+    UserInputError,
+)
+
+from lib.bot import Bot, Cog, CommandError, Interaction
 from lib.converters import CommaList
 
 ERROR_LOG_FORMAT = """
@@ -13,66 +23,101 @@ Feel free to report this to @{owner} Lol
 
 
 class Cogs(Cog):
-    @slash_command()
-    async def reload_exts(
-        self, inter: Interaction, extensions: list[str] = CommaList()
+    async def ext_command_handler(
+        self,
+        inter: Interaction,
+        func: Callable[[str], None],
+        action: str,
+        exts: list[str],
     ) -> None:
-        """
-        Reloads given extensions.
+        if not exts:
+            raise UserInputError(f"couldn't figure out which extensions to {action}")
 
-        If no extensions are given, reloads all extensions.
-        """
         await inter.send(
-            "Reloading extensions . . .", ephemeral=self.settings.hide_mod_commands
+            f"{action} extensions . . .", ephemeral=self.settings.hide_mod_commands
         )
 
-        if not extensions:
-            extensions = list(self.bot.extensions)
-        logging.info(f"{extensions}")
-
         failed = 0
-        total = len(extensions)
+        total = len(exts)
 
         log = "```\n"
 
-        for ext in extensions:
+        for ext in exts:
             try:
-                self.bot.reload_extension(ext)
+                func(ext)
             except Exception as exc:
-                m = (
-                    f"Failed to reload extension '{ext}':\n"
+                failed += 1
+                msg = (
+                    f"Failed to {action} extension '{ext}':\n"
                     f"\t{type(exc).__name__}: {exc}"
                 )
-                logging.exception(m)
-                failed += 1
+
+                # some exceptions are not worth looking at in the console
+                if not isinstance(
+                    exc, (ExtensionAlreadyLoaded, ExtensionNotLoaded, ExtensionNotFound)
+                ):
+                    logging.exception(msg)
             else:
-                m = f"Managed to reload extension '{ext}'"
-                logging.debug(m)
-            log += m
+                msg = f"Managed to {action} extension '{ext}'"
+                logging.debug(msg)
+
+            log += msg
             log += "\n"
 
         if failed == 0:
-            final_log = "All extensions reloaded successfully!"
+            final_log = f"All extensions {action}ed successfully!"
         else:
             ratio = failed * 10_000 // total / 100
-            final_log = f"{ratio}% of extensions failed to reload."
+            final_log = f"{ratio}% of extensions failed to {action}."
 
         logging.debug(final_log)
         await inter.edit_original_response(log + "```\n" + final_log)
+
+    @commands.slash_command()
+    @commands.is_owner()
+    async def load_extensions(
+        self, inter: Interaction, extensions: list[str] = CommaList()
+    ) -> None:
+        """Loads given extensions."""
+        await self.ext_command_handler(inter, self.bot.load_extension, "load", extensions)
+
+    @commands.slash_command()
+    @commands.is_owner()
+    async def reload_extensions(
+        self, inter: Interaction, extensions: list[str] = CommaList()
+    ) -> None:
+        """Reloads given extensions. If none are given, defaults to all."""
+        extensions = extensions or list(self.bot.extensions)
+        await self.ext_command_handler(
+            inter, self.bot.reload_extension, "reload", extensions
+        )
+
+    @commands.slash_command()
+    @commands.is_owner()
+    async def unload_extensions(
+        self, inter: Interaction, extensions: list[str] = CommaList()
+    ) -> None:
+        """Unloads given extensions."""
+        await self.ext_command_handler(
+            inter, self.bot.reload_extension, "unload", extensions
+        )
 
     @Cog.listener()
     async def on_slash_command_error(
         self, inter: Interaction, error: CommandError
     ) -> None:
-        msg = "\n".join(traceback.format_exception(error))
+        if isinstance(error, NotOwner | UserInputError):
+            msg = type(error).__name__ + " error: " + str(error)
+            await inter.send(msg)
+        else:
+            msg = "\n".join(traceback.format_exception(error))
 
-        if len(msg) > 1950:
-            index = msg.rfind("\n", 0, 1950)
-            msg = msg[:index] + "\n..."
+            if len(msg) > 1950:
+                index = msg.rfind("\n", 0, 1950)
+                msg = msg[:index] + "\n..."
 
-        msg = ERROR_LOG_FORMAT.format(contents=msg, owner=self.app_info.owner)
-
-        await inter.send(msg, ephemeral=True)
+            msg = ERROR_LOG_FORMAT.format(contents=msg, owner=self.app_info.owner)
+            await inter.send(msg, ephemeral=True)
 
 
 def setup(bot: Bot) -> None:
